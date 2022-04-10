d27982578986cec49befd91f25f40731cc2abc97
kovalyov-z3 <71659839+kovalyov-z3@users.noreply.github.com>
2022-04-10 18:54:02 +0300

    bot asyncronased major architecture changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from cgitb import text
-import telebot
-from telebot import types
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 import urlworker
-from architecture_articles_parser import get_random_article_from_architect4u
-token = "5118191064:AAGkLtD7YJkPMXV2uZaCp4M0txgEOO5KFQ8"
-bot = telebot.TeleBot(token)
-p = urlworker.Parser("https://mgsu.ru/student/Raspisanie_zanyatii_i_ekzamenov/fayly-raspisaniya-dlya-skachivaniya/")
+
+TOKEN = "5118191064:AAGkLtD7YJkPMXV2uZaCp4M0txgEOO5KFQ8"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 BOT_COMMANDS = ["Расписание на сегодня", "Расписание на завтра", "Какая сейчас неделя?", "Какая сейчас пара по счету?",
                 "Статья об архитектуре"]
@@ -13,30 +14,39 @@ BOT_BUTTONS = []
 for command in BOT_COMMANDS:
     BOT_BUTTONS.append(types.KeyboardButton(command))
 
-@bot.message_handler(commands=["start"])
-def start_message(message):
+mgsu_parser = urlworker.ParserFactory.create_parser_instance("MGSU")
+articles_parser = urlworker.ParserFactory.create_parser_instance("ARTICLE")
+
+@dp.message_handler(commands=['start', 'help'])
+async def process_start_command(msg: types.Message):
     markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     for button in BOT_BUTTONS:
         markup.add(button)
-    bot.send_message(message.chat.id, "Выберите нужную кнопку:", reply_markup=markup)
+    await bot.send_message(msg.from_user.id, "Выберите нужную кнопку:", reply_markup=markup)
 
 
-@bot.message_handler(content_types=["text"])
-def message_reply(message):
+
+@dp.message_handler(content_types=["text"])
+async def message_reply(message):
     if (message.text == BOT_COMMANDS[0]):
-        bot.send_message(message.chat.id, "1")
+        await bot.send_message(message.chat.id, "1")
 
     elif (message.text == BOT_COMMANDS[1]):
-        bot.send_message(message.chat.id, "2")
+        await bot.send_message(message.chat.id, "2")
 
     elif (message.text == BOT_COMMANDS[2]):
-        bot.send_message(message.chat.id, p.set_week())
+        text =  await mgsu_parser.set_week()
+        await bot.send_message(message.chat.id, text)
 
     elif (message.text == BOT_COMMANDS[3]):
-        bot.send_message(message.chat.id, "4")
+        await bot.send_message(message.chat.id, "4")
 
     elif (message.text == BOT_COMMANDS[4]):
-        bot.send_message(message.chat.id, get_random_article_from_architect4u())
+        text = await articles_parser.get_random_article()
+        await bot.send_message(message.chat.id, text)
 
 
-bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
