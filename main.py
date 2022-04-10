2c7b5a69d3870a71f6fefe339e6645dc222fd30d
uttsvlad <uttsvlad@gmail.com>
2022-04-10 14:38:21 +0300

    Sharing project

---
+++ main.py
@@ -0,0 +1,39 @@
+import telebot
+from telebot import types
+
+token = '5118191064:AAGkLtD7YJkPMXV2uZaCp4M0txgEOO5KFQ8'
+bot = telebot.TeleBot(token)
+
+BOT_COMMANDS = ['Расписание на сегодня', 'Расписание на завтра', 'Какая сейчас неделя?', 'Какая сейчас пара по счету?',
+                'Архитектурная статья дня']
+BOT_BUTTONS = []
+for command in BOT_COMMANDS:
+    BOT_BUTTONS.append(types.KeyboardButton(command))
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    for button in BOT_BUTTONS:
+        markup.add(button)
+    bot.send_message(message.chat.id, 'Выберите нужную кнопку:', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if (message.text == BOT_COMMANDS[0]):
+        bot.send_message(message.chat.id, "1")
+
+    elif (message.text == BOT_COMMANDS[1]):
+        bot.send_message(message.chat.id, "2")
+
+    elif (message.text == BOT_COMMANDS[2]):
+        bot.send_message(message.chat.id, "3")
+
+    elif (message.text == BOT_COMMANDS[3]):
+        bot.send_message(message.chat.id, "4")
+
+    elif (message.text == BOT_COMMANDS[4]):
+        bot.send_message(message.chat.id, "5")
+
+
+bot.infinity_polling()
