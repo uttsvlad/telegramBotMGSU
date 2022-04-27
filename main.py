6a4e238735d5bade11780c81f42913d490107d99
kovalyov-z3 <71659839+kovalyov-z3@users.noreply.github.com>
2022-04-27 11:21:08 +0300

    major architecture changes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import telebot
 import urlworker
+from telebot import types
 
 TOKEN = "5118191064:AAGkLtD7YJkPMXV2uZaCp4M0txgEOO5KFQ8"
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+bot = telebot.TeleBot(TOKEN)
 
 BOT_COMMANDS = ["Расписание на сегодня", "Расписание на завтра", "Какая сейчас неделя?", "Какая сейчас пара по счету?",
                 "Статья об архитектуре"]
@@ -18,35 +16,36 @@ mgsu_parser = urlworker.ParserFactory.create_mgsu_parser()
 articles_parser = urlworker.ParserFactory.create_articles_parser()
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def process_start_command(msg: types.Message):
+@bot.message_handler(commands=['start', 'help'])
+def process_start_command(msg: types.Message):
+    
     markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     for button in BOT_BUTTONS:
         markup.add(button)
-    await bot.send_message(msg.from_user.id, "Выберите нужную кнопку:", reply_markup=markup)
+    bot.send_message(msg.from_user.id, "Выберите нужную кнопку:", reply_markup=markup)
 
 
-@dp.message_handler(content_types=["text"])
-async def message_reply(message):
+@bot.message_handler(content_types=["text"])
+def message_reply(message):
     if message.text == BOT_COMMANDS[0]:
-        await bot.send_message(message.chat.id, "1")
+        bot.send_message(message.chat.id, "1")
 
     elif message.text == BOT_COMMANDS[1]:
-        await bot.send_message(message.chat.id, "2")
+        bot.send_message(message.chat.id, "2")
 
     elif message.text == BOT_COMMANDS[2]:
 
-        text = await mgsu_parser.set_week()
-        await bot.send_message(message.chat.id, text)
+        text = mgsu_parser.set_week()
+        bot.send_message(message.chat.id, text)
 
     elif message.text == BOT_COMMANDS[3]:
-        await bot.send_message(message.chat.id, "4")
+        bot.send_message(message.chat.id, "4")
 
     elif message.text == BOT_COMMANDS[4]:
 
-        text = await articles_parser.get_random_article()
-        await bot.send_message(message.chat.id, text)
+        text = articles_parser.get_random_article()
+        bot.send_message(message.chat.id, text)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    bot.polling(none_stop=True, interval=0)
--- urlworker.py
+++ urlworker.py
@@ -7,47 +7,45 @@ URLS = ["https://mgsu.ru/student/Raspisanie_zanyatii_i_ekzamenov/fayly-raspisani
         "https://www.architect4u.ru/articles-3.html"]
 
 
-def check_status(self):
-    if str(self.__r.status_code)[0] == '5':
-        return "Ошибка сервера!"
-
-    elif str(self.__r.status_code) == '404':
-        return "Страница не найдена!"
-
-    return "Нет информации, возможно, на сайте ведутся технические работы"
-
-
 class MGSUParser:
-    def __init__(self):
-        self.__r = requests.get(URLS[0], verify=False)
-        self.__soup = bs(self.__r.text, "html.parser")
-        self.__week = self.__soup.find_all('h4')
+    def __init__(self, url = URLS[0]):
+        self.r = requests.get(url, verify=False)
+        self.soup = bs(self.r.text, "html.parser")
+        self.week = self.soup.find_all('h4')
 
-    async def set_week(self):
-        if self.__r.status_code == 200:
-            for data in self.__week:
+    def check_status(self):
+        if str(self.r.status_code)[0] == '5':
+            return "Ошибка сервера!"
+
+        elif str(self.r.status_code) == '404':
+            return "Страница не найдена!"
+
+        return "Нет информации, возможно, на сайте ведутся технические работы"
+
+    def set_week(self):
+        if self.r.status_code == 200:
+            for data in self.week:
                 if (data.find('b') is not None) and ("неделя" in str(data.text)):
                     return data.text
 
         else:
-            check_status(self)
+            self.check_status()
 
 
-class ArticlesParser:
+class ArticlesParser(MGSUParser):
     def __init__(self):
-        self.__r = requests.get(URLS.pop(randint(1, len(URLS) - 1)))
-        self.__soup = bs(self.__r.text, "html.parser")
-        self.__articles = self.__soup.findAll("div", class_="card")
+        super().__init__(url = URLS.pop(randint(1, len(URLS) - 1)))
+        self.articles = self.soup.findAll("div", class_="card")
 
-    async def get_random_article(self):
-        if self.__r.status_code == 200:
+    def get_random_article(self):
+        if self.r.status_code == 200:
             articles_hrefs = []
-            for article in self.__articles:
+            for article in self.articles:
                 articles_hrefs.append(article.a['href'])
             return "https://www.architect4u.ru/" + articles_hrefs.pop(randint(0, len(articles_hrefs) - 1))
 
         else:
-            check_status(self)
+            self.check_status()
 
 
 class ParserFactory:
